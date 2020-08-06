1dff1352ee98d24c1b9381731ce1a1ed2671f7b5
undisputedashu <ashutosh.4040@gmail.com>
2020-08-06 23:56:39 +0530

    add

---
+++ src/com/aj/dp/codeforces/NewYear.java
@@ -0,0 +1,19 @@
+package com.aj.dp.codeforces;
+
+import java.util.Scanner;
+
+public class NewYear {
+
+	private static long mod = 998244353 ;
+	public static void main(String args[]) {
+		Scanner in = new Scanner(System.in);
+		long n = in.nextLong();
+		findAndPrint(n);
+		in.close();
+	}
+
+	private static void findAndPrint(long n) {
+		
+	}
+	
+}
